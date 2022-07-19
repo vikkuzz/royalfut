199a6c1911eae1374bde90cc1c914daa01bf7a1f
Viktor <simpat007@yandex.ru>
2022-07-20 00:12:30 +0300

    background & header shadow

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,11 @@
-import './App.scss';
+import "./App.scss";
 
-import Header from '../Header';
-import Calculator from '../Calculator';
+import Header from "../Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <h1 className="App__title">Safest option to buy FIFA 22 coins</h1>
-      {/* <Calculator/> */}
+      <Header />
     </div>
   );
 }
